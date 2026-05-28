d54448224ca5611f0ad5d590b7ab9c3cf6951155
Tamim7xd <152809345+Tamim7xd@users.noreply.github.com>
2026-05-29 02:48:02 +0300

    Update db.py

--- db.py
+++ db.py
@@ -1,3 +1,8 @@
+import sqlite3
+
+conn = sqlite3.connect("bot.db", check_same_thread=False)
+c = conn.cursor()
+
 c.execute("""
 CREATE TABLE IF NOT EXISTS users (
     user_id INTEGER PRIMARY KEY,
@@ -7,8 +12,11 @@ CREATE TABLE IF NOT EXISTS users (
     xp INTEGER DEFAULT 0,
     level INTEGER DEFAULT 1,
     title TEXT DEFAULT 'مبتدئ',
+    muted INTEGER DEFAULT 0,
     banned INTEGER DEFAULT 0,
-    muted INTEGER DEFAULT 0
+    vip INTEGER DEFAULT 0,
+    last_daily INTEGER DEFAULT 0
 )
 """)
+
 conn.commit()
