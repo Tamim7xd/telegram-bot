4c88f7703403dd7adfaf6a59b25a62ff0758178f
Tamim7xd <152809345+Tamim7xd@users.noreply.github.com>
2026-05-28 21:15:46 +0300

    Update users.py

--- core/users.py
+++ core/users.py
@@ -69,12 +69,16 @@ TITLES = [
 # ─────────────────────────────
 def register(user):
 
-    c.execute("SELECT user_id FROM users WHERE user_id=?", (user.id,))
+    c.execute(
+        "SELECT user_id FROM users WHERE user_id=?",
+        (user.id,)
+    )
+
     if not c.fetchone():
 
         c.execute("""
-        INSERT INTO users(user_id, name)
-        VALUES (?,?)
+            INSERT INTO users(user_id, name)
+            VALUES (?, ?)
         """, (user.id, user.first_name))
 
         conn.commit()
@@ -85,7 +89,11 @@ def register(user):
 # ─────────────────────────────
 def get(uid):
 
-    c.execute("SELECT * FROM users WHERE user_id=?", (uid,))
+    c.execute(
+        "SELECT * FROM users WHERE user_id=?",
+        (uid,)
+    )
+
     return c.fetchone()
 
 
@@ -95,10 +103,10 @@ def get(uid):
 def add_message(uid):
 
     c.execute("""
-    UPDATE users
-    SET messages = messages + 1,
-        money = money + 250
-    WHERE user_id=?
+        UPDATE users
+        SET messages = messages + 1,
+            money = money + 250
+        WHERE user_id=?
     """, (uid,))
 
     conn.commit()
