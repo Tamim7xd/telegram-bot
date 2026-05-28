39ad44a5daae15b24b3a13ce5dd7c5e21a50f920
Tamim7xd <152809345+Tamim7xd@users.noreply.github.com>
2026-05-28 17:27:36 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,11 @@
 import os
 
 TOKEN = os.getenv("BOT_TOKEN")
-GROUP_ID = int(os.getenv("GROUP_ID", "0"))
-ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
+
+GROUP_ID = int(
+    os.getenv("GROUP_ID", "0")
+)
+
+ADMIN_ID = int(
+    os.getenv("ADMIN_ID", "0")
+)
