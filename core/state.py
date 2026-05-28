63dbf9ca9083242825492e31bf5010e4fe23edca
Tamim7xd <152809345+Tamim7xd@users.noreply.github.com>
2026-05-28 21:23:52 +0300

    Update state.py

--- core/state.py
+++ core/state.py
@@ -1,11 +1,10 @@
 STATE = {}
 
-def set_state(admin_id, action, target):
-    STATE[admin_id] = {"action": action, "target": target}
+def set_state(admin, action, target):
+    STATE[admin] = {"action": action, "target": target}
 
-def get_state(admin_id):
-    return STATE.get(admin_id)
+def get_state(admin):
+    return STATE.get(admin)
 
-def clear_state(admin_id):
-    if admin_id in STATE:
-        del STATE[admin_id]
+def clear_state(admin):
+    STATE.pop(admin, None)
