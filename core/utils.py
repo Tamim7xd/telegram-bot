e26321b1c531856cbd473b330f6656b2b0258cd8
Tamim7xd <152809345+Tamim7xd@users.noreply.github.com>
2026-05-29 01:09:38 +0300

    Update utils.py

--- core/utils.py
+++ core/utils.py
@@ -1,8 +1,4 @@
 def format_iq_money(amount: int) -> str:
-
-    # أقل من 1000
     if amount < 1000:
         return f"{amount} دينار عراقي"
-
-    # 1000 أو أكثر
     return f"{amount:,} دينار عراقي"
