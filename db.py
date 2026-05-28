7600fde65629cf2861398fa14864ad9d07ddf03e
Tamim7xd <152809345+Tamim7xd@users.noreply.github.com>
2026-05-28 21:06:35 +0300

    Update db.py

--- db.py
+++ db.py
@@ -4,39 +4,27 @@ conn = sqlite3.connect("bot.db", check_same_thread=False)
 c = conn.cursor()
 
 
-# ─────────────────────────────
-# 🧠 إنشاء قاعدة البيانات
-# ─────────────────────────────
 def init_db():
 
-    # ───────── USERS (النظام الكامل) ─────────
     c.execute("""
     CREATE TABLE IF NOT EXISTS users(
-
         user_id INTEGER PRIMARY KEY,
         name TEXT,
-
         messages INTEGER DEFAULT 0,
         money INTEGER DEFAULT 0,
-
         xp INTEGER DEFAULT 0,
         level INTEGER DEFAULT 1,
-
         warns INTEGER DEFAULT 0,
         rewards INTEGER DEFAULT 0,
-
         title TEXT DEFAULT 'مبتدئ',
         title_locked INTEGER DEFAULT 0,
-
         banned INTEGER DEFAULT 0,
         muted INTEGER DEFAULT 0
     )
     """)
 
-    # ───────── QUESTIONS ─────────
     c.execute("""
     CREATE TABLE IF NOT EXISTS questions(
-
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         question TEXT,
         answer TEXT
