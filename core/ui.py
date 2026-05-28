070e061113e67c1e4beed2b2ca061f18f98a37d9
Tamim7xd <152809345+Tamim7xd@users.noreply.github.com>
2026-05-29 00:29:07 +0300

    Update ui.py

--- core/ui.py
+++ core/ui.py
@@ -1,3 +1,9 @@
+u = get(uid)
+
+if not u:
+    await query.edit_message_text("❌ المستخدم غير موجود")
+    return
+
 await query.edit_message_text(
 f"""
 ━━━━━━━━━━━━━━
@@ -8,16 +14,16 @@ f"""
 👤 الاسم: {u[1]}
 
 💰 المال:
-{format_iq_money(u[3])}
+{format_iq_money(u[3] or 0)}
 
 📨 عدد الرسائل:
-{u[2]:,}
+{u[2] or 0:,}
 
 ⭐ المستوى:
-{u[4]}
+{u[4] or 1}
 
 🏆 اللقب:
-{u[8]}
+{u[8] or "بدون لقب"}
 
 ━━━━━━━━━━━━━━
 📊 الحالة
