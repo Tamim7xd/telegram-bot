33dd20e09c59d5d762656d9c80227bcc6c49114b
Tamim7xd <152809345+Tamim7xd@users.noreply.github.com>
2026-05-28 23:35:48 +0300

    Update users.py

--- core/users.py
+++ core/users.py
@@ -1,13 +1,44 @@
-from db import c, conn
+from db import conn, c
+
 
 def register(user):
-    c.execute("SELECT user_id FROM users WHERE user_id=?", (user.id,))
+
+    c.execute(
+        "SELECT user_id FROM users WHERE user_id=?",
+        (user.id,)
+    )
+
     if not c.fetchone():
-        c.execute("INSERT INTO users(user_id, name) VALUES(?,?)",
-                  (user.id, user.first_name))
+
+        c.execute(
+            "INSERT INTO users(user_id, name) VALUES(?,?)",
+            (user.id, user.first_name)
+        )
+
         conn.commit()
 
 
+# =========================
+# GET USER
+# =========================
 def get(uid):
-    c.execute("SELECT * FROM users WHERE user_id=?", (uid,))
+
+    c.execute(
+        "SELECT * FROM users WHERE user_id=?",
+        (uid,)
+    )
+
     return c.fetchone()
+
+
+# =========================
+# ADD MESSAGE
+# =========================
+def add_message(uid):
+
+    c.execute(
+        "UPDATE users SET messages = messages + 1 WHERE user_id=?",
+        (uid,)
+    )
+
+    conn.commit()
