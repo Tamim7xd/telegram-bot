bd22c9b5d79e406438f47aeee9a2cd6ff4649eff
Tamim7xd <152809345+Tamim7xd@users.noreply.github.com>
2026-05-28 20:42:03 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,5 @@
 import os
 
 TOKEN = os.getenv("BOT_TOKEN")
-
 ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
 GROUP_ID = int(os.getenv("GROUP_ID", "0"))
-
-if not TOKEN:
-    raise ValueError("BOT_TOKEN missing")
