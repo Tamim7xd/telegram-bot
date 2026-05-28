1dae15e762c13df1c5b1f80edf10f9690614d8b5
Tamim7xd <152809345+Tamim7xd@users.noreply.github.com>
2026-05-28 19:43:30 +0300

    Update actions.py

--- core/actions.py
+++ core/actions.py
@@ -1,1 +1,25 @@
+from db import c, conn
 
+def add_money(uid, amount):
+    c.execute("UPDATE users SET money = money + ? WHERE user_id=?", (amount, uid))
+    conn.commit()
+
+def remove_money(uid, amount):
+    c.execute("UPDATE users SET money = money - ? WHERE user_id=?", (amount, uid))
+    conn.commit()
+
+def set_title(uid, title):
+    c.execute("UPDATE users SET title=? WHERE user_id=?", (title, uid))
+    conn.commit()
+
+def mute(uid):
+    c.execute("UPDATE users SET warns = warns + 1 WHERE user_id=?", (uid,))
+    conn.commit()
+
+def ban(uid):
+    c.execute("UPDATE users SET banned=1 WHERE user_id=?", (uid,))
+    conn.commit()
+
+def unban(uid):
+    c.execute("UPDATE users SET banned=0 WHERE user_id=?", (uid,))
+    conn.commit()
