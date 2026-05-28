fca5db768e6ca36d44035cc9c25b13171081fe87
Tamim7xd <152809345+Tamim7xd@users.noreply.github.com>
2026-05-29 02:51:38 +0300

    Delete core/ui.py

--- core/ui.py
+++
@@ -1,7 +0,0 @@
-from telegram import InlineKeyboardMarkup, InlineKeyboardButton
-
-def admin_menu():
-    return InlineKeyboardMarkup([
-        [InlineKeyboardButton("👥 الأعضاء", callback_data="users")],
-        [InlineKeyboardButton("📊 الإحصائيات", callback_data="stats")]
-    ])
