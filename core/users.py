ad9b9796816e551618c058d32badb6174cfb0b15
Tamim7xd <152809345+Tamim7xd@users.noreply.github.com>
2026-05-29 02:07:32 +0300

    Update users.py

--- core/users.py
+++ core/users.py
@@ -1,25 +1,24 @@
 from db import c, conn
-
-def register(user):
-    c.execute("SELECT user_id FROM users WHERE user_id=?", (user.id,))
-    if not c.fetchone():
-        c.execute(
-            "INSERT INTO users(user_id, name) VALUES(?,?)",
-            (user.id, user.first_name)
-        )
-        conn.commit()
+from core.xp import level_up, need_xp
+from core.titles import TITLES
 
 
-def get(uid):
-    c.execute("SELECT * FROM users WHERE user_id=?", (uid,))
-    return c.fetchone()
+def add_xp(user_id, amount):
+    c.execute("SELECT xp, level FROM users WHERE user_id=?", (user_id,))
+    row = c.fetchone()
 
+    if not row:
+        return
 
-def add_message(uid):
+    xp, level = row
+    xp += amount
+
+    level, xp, title = level_up(level, xp, TITLES)
+
     c.execute("""
         UPDATE users
-        SET messages = messages + 1,
-            money = money + 250
+        SET xp=?, level=?, title=?
         WHERE user_id=?
-    """, (uid,))
+    """, (xp, level, title, user_id))
+
     conn.commit()
