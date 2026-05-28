6b1ff0c99c03fb9220c7d9bb55e7a956b0765bd6
Tamim7xd <152809345+Tamim7xd@users.noreply.github.com>
2026-05-28 13:23:04 +0300

    Update db.py

--- db.py
+++ db.py
@@ -1,23 +1,28 @@
 import sqlite3
-import time
 
 conn = sqlite3.connect("bot.db", check_same_thread=False)
 c = conn.cursor()
 
-c.execute("""
-CREATE TABLE IF NOT EXISTS users(
-user_id INTEGER PRIMARY KEY,
-name TEXT,
-money INTEGER DEFAULT 0,
-messages INTEGER DEFAULT 0,
-title TEXT DEFAULT 'مبتدئ',
-start_time INTEGER,
-banned INTEGER DEFAULT 0,
-muted_until INTEGER DEFAULT 0
-)
-""")
+def init_db():
+    c.execute("""
+    CREATE TABLE IF NOT EXISTS users(
+        user_id INTEGER PRIMARY KEY,
+        name TEXT,
+        money INTEGER DEFAULT 0,
+        messages INTEGER DEFAULT 0,
+        role TEXT DEFAULT 'user',
+        locked INTEGER DEFAULT 0
+    )
+    """)
 
-conn.commit()
+    c.execute("""
+    CREATE TABLE IF NOT EXISTS questions(
+        user_id INTEGER PRIMARY KEY,
+        answer TEXT
+    )
+    """)
 
-def now():
-    return int(time.time())
+    conn.commit()
+
+def db():
+    return conn, c
