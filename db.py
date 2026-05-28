98bf778939763a64e2e6a29b2e59474566170831
Tamim7xd <152809345+Tamim7xd@users.noreply.github.com>
2026-05-28 17:28:11 +0300

    Update db.py

--- db.py
+++ db.py
@@ -1,28 +1,43 @@
 import sqlite3
 
-conn = sqlite3.connect("bot.db", check_same_thread=False)
+conn = sqlite3.connect(
+    "bot.db",
+    check_same_thread=False
+)
+
 c = conn.cursor()
 
+
 def init_db():
+
     c.execute("""
     CREATE TABLE IF NOT EXISTS users(
+
         user_id INTEGER PRIMARY KEY,
+
         name TEXT,
-        xp INTEGER DEFAULT 0,
+
         messages INTEGER DEFAULT 0,
-        role TEXT DEFAULT 'user',
-        locked INTEGER DEFAULT 0
+
+        money INTEGER DEFAULT 0,
+
+        warnings INTEGER DEFAULT 0,
+
+        rewards INTEGER DEFAULT 0,
+
+        custom_title TEXT DEFAULT '',
+
+        title_locked INTEGER DEFAULT 0
     )
     """)
 
     c.execute("""
     CREATE TABLE IF NOT EXISTS questions(
+
         user_id INTEGER PRIMARY KEY,
+
         answer TEXT
     )
     """)
 
     conn.commit()
-
-def db():
-    return conn, c
