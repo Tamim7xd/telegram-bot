470066e25f72abf4ad0438f49713b2a3a6547285
Tamim7xd <152809345+Tamim7xd@users.noreply.github.com>
2026-05-28 13:26:30 +0300

    Update admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,2 +1,13 @@
-def admin():
-    print("admin loaded")
+from db import db
+from config import GROUP_ID
+
+conn, c = db()
+
+def is_admin(role):
+    return role in ["admin","owner"]
+
+async def notify(bot, text):
+    try:
+        await bot.send_message(GROUP_ID, f"🔔 {text}")
+    except:
+        pass
