8367c7a3e424488e6bc68d70301b56355fc8ef8d
Tamim7xd <152809345+Tamim7xd@users.noreply.github.com>
2026-05-29 06:06:43 +0300

    Update titles.py

--- _core/titles.py
+++ _core/titles.py
@@ -1,1 +1,10 @@
+from db import db
 
+async def set_user_title(telegram_id: int, title: str):
+    await db.execute("UPDATE users SET title = $1 WHERE telegram_id = $2", title, telegram_id)
+
+async def remove_user_title(telegram_id: int):
+    await db.execute("UPDATE users SET title = '' WHERE telegram_id = $1", telegram_id)
+
+def register_titles_handlers(dp):
+    pass
