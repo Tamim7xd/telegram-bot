b57ca09f840555bc22c2dcfd55842fc6e9f3e449
Tamim7xd <152809345+Tamim7xd@users.noreply.github.com>
2026-05-29 12:08:07 +0300

    Update xp.py

--- _core/xp.py
+++ _core/xp.py
@@ -1,2 +1,29 @@
-async def add_xp(user_id, amount, chat_id, name):
-    pass
+from Core_.users import get_user
+from Core_.notify import send_notify
+
+
+async def add_xp(uid, name):
+
+    u = await get_user(uid)
+
+    u["xp"] += 5
+    u["money"] += 50
+
+    need = u["level"] * 100
+
+    leveled = False
+
+    if u["xp"] >= need:
+        u["xp"] -= need
+        u["level"] += 1
+        leveled = True
+
+    if leveled:
+        await send_notify(
+            uid,
+            "LEVEL UP",
+            f"🎉 وصلت مستوى {u['level']}\n💰 +50 مكافأة\n🏆 استمر!",
+            "🔥"
+        )
+
+    return leveled
