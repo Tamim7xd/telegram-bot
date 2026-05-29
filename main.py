1bc5852a0f2a1c006a6752299656579273053d95
Tamim7xd <152809345+Tamim7xd@users.noreply.github.com>
2026-05-29 12:14:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-from aiogram import Bot, Dispatcher
 import asyncio
+from aiogram import Bot, Dispatcher
 
 from config import BOT_TOKEN
-from _core.bot_core import setup_bot
-from _core.notify import set_bot_instance
+from Core_.bot_core import setup
+
 
 async def main():
+
     bot = Bot(token=BOT_TOKEN)
     dp = Dispatcher()
 
-    set_bot_instance(bot)
-
-    setup_bot(dp)
+    setup(dp)
 
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
