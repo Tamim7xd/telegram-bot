916754b6fb6bce6346ca077c788abbb0819a25d6
Tamim7xd <152809345+Tamim7xd@users.noreply.github.com>
2026-05-29 04:45:14 +0300

    Update state.py

--- core/state.py
+++ core/state.py
@@ -1,19 +1,1 @@
-states = {}
 
-
-def set_state(uid, action, target):
-
-    states[uid] = {
-        "action": action,
-        "target": target
-    }
-
-
-def get_state(uid):
-    return states.get(uid)
-
-
-def clear_state(uid):
-
-    if uid in states:
-        del states[uid]
