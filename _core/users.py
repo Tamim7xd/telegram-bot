d8ab1cd8c9ad6f9ab9f441c2761ff24f5ca9a003
Tamim7xd <152809345+Tamim7xd@users.noreply.github.com>
2026-05-30 08:57:56 +0300

    Update users.py

--- _core/users.py
+++ _core/users.py
@@ -1,17 +1,39 @@
-from db import execute, fetchone
+from db import db
+from aiogram.types import User
+from config import STARTING_MONEY, STARTING_XP, ADMIN_IDS
 
-async def get_user(uid):
-    row = fetchone("SELECT * FROM users WHERE telegram_id=?", (uid,))
-    return row
 
-async def create_user(user):
-    execute(
-        "INSERT OR IGNORE INTO users (telegram_id, username, full_name) VALUES (?, ?, ?)",
-        (user.id, user.username, user.full_name)
+async def is_admin(user_id: int):
+    return user_id in ADMIN_IDS
+
+
+async def get_or_create_user(tg_user: User):
+    row = await db.fetchrow(
+        "SELECT * FROM users WHERE telegram_id = ?",
+        tg_user.id
     )
 
-async def add_money(uid, amount):
-    execute("UPDATE users SET money = money + ? WHERE telegram_id=?", (amount, uid))
+    if row:
+        return row
 
-async def add_xp(uid, xp):
-    execute("UPDATE users SET xp = xp + ? WHERE telegram_id=?", (xp, uid))
+    await db.execute(
+        "INSERT INTO users (telegram_id, username, full_name, money, xp) VALUES (?, ?, ?, ?, ?)",
+        tg_user.id,
+        tg_user.username,
+        tg_user.full_name,
+        STARTING_MONEY,
+        STARTING_XP
+    )
+
+    return await db.fetchrow(
+        "SELECT * FROM users WHERE telegram_id = ?",
+        tg_user.id
+    )
+
+
+async def update_user_money(uid: int, delta: int, reason="", admin_id=None):
+    await db.execute(
+        "UPDATE users SET money = money + ? WHERE telegram_id = ?",
+        delta,
+        uid
+    )
