0a29c9609e51b80c34c0aba52206ad8267cae92c
Tamim7xd <152809345+Tamim7xd@users.noreply.github.com>
2026-05-31 12:50:26 +0300

    Create shop_data.py

---
+++ system_shop/shop_data.py
@@ -0,0 +1,14 @@
+SHOP_TIMEOUT = 3
+
+TITLES = {
+    1: {"name": "عضو جديد 🌱", "price": 1000},
+    2: {"name": "مقاتل ⚔️", "price": 2500},
+    3: {"name": "ملك 👑", "price": 5000},
+    4: {"name": "VIP 💎", "price": 10000},
+    5: {"name": "أسطوري 🔥", "price": 20000},
+}
+
+SHOP_MENU = [
+    {"name": f"{data['name']} - {data['price']} 🪙", "id": tid}
+    for tid, data in TITLES.items()
+]
