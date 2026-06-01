a6bdd8e7481e863f3df59e00b65dd7e7155d5ba1
Tamim7xd <152809345+Tamim7xd@users.noreply.github.com>
2026-06-01 06:05:22 +0300

    Update time_manager.py

--- shared/time_manager.py
+++ shared/time_manager.py
@@ -1,1 +1,10 @@
- 
+import time
+
+def get_timestamp():
+    return int(time.time())
+
+def format_time(timestamp):
+    return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(timestamp))
+
+def is_expired(until_timestamp):
+    return get_timestamp() > until_timestamp 
